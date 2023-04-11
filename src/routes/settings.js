8952bc523a0683182c58a4a6795c631b8fb31da4
beyond88 <muhin.cse.diu@gmail.com>
2023-04-12 00:19:34 +0600

    Remove extra codes for file upload

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -1,5 +1,4 @@
 const settingsdRouter = require("express").Router();
-const fileUpload = require('express-fileupload');
 
 const {
     getByIdHandler,
@@ -20,15 +19,6 @@ const {
 
 const auth = require("../middleware/auth");
 
-settingsdRouter.use(
-    fileUpload({
-        limits: {
-            fileSize: 10000000,
-        },
-        abortOnLimit: true,
-    })
-);
-
 settingsdRouter.get("/", auth, async (req, res) => {
     const item = await getById(req.user._id, 'users');
     res.render('pages/profile', {item: item});
@@ -43,24 +33,6 @@ settingsdRouter.put("/update", (req, res, next) => {
     return res.status(200).send({
         res: item
     });
-})
-
-settingsdRouter.post("/avatar", (req, res, next) => {
-    
-    // Get the file that was set to our field named "image"
-    const { image } = req.files;
-
-    // If no image submitted, exit
-    if (!image) return res.sendStatus(400);
-
-    // If does not have image mime type prevent from uploading
-    if (/^image/.test(image.mimetype)) return res.sendStatus(400);
-
-    // Move the uploaded image to our upload folder
-    image.mv(__dirname + '/upload/' + image.name);
-
-    // All good
-    res.status(200).send({status:'okk', message: 'Uploaded'});
 })
 
 module.exports = settingsdRouter;
