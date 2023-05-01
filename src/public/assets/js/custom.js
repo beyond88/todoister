e921ff6bc7e765a12a1a0e97e5766804aac36cd8
beyond88 <muhin.cse.diu@gmail.com>
2023-05-01 13:55:31 +0600

    Added lastLogin field in user model

--- src/public/assets/js/custom.js
+++ src/public/assets/js/custom.js
@@ -1,18 +1,3 @@
-
-
-
-/*=============================================================
-    Authour URI: www.binarycart.com
-    License: Commons Attribution 3.0
-
-    http://creativecommons.org/licenses/by/3.0/
-
-    100% To use For Personal And Commercial Use.
-    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
-   
-    ========================================================  */
-
-
 (function ($) {
     "use strict";
     var mainApp = {
@@ -331,4 +316,18 @@
 
     });
 
+    $.fn.checkCooke = function(name) {
+        var match = document.cookie.match(RegExp('(?:^|;\\s*)' + name + '=([^;]*)')); 
+        return match ? match[1] : null;
+    }
+
+    if( $.fn.checkCooke('lastLogin') ) {
+
+        let lastLogin = $.fn.checkCooke('lastLogin');
+        console.log('I got it ==>', lastLogin);
+
+    }
+
 }(jQuery));
+
+
